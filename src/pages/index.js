77dc20eee22103ab86b590dfde92ab02686fc713
Caleb Espinoza <caleb.espinoza@outlook.com>
2023-06-09 00:57:25 -0400

    Add source code

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+//import Image from 'next/image'
+//import { Inter } from 'next/font/google'
+//import styles from '@/styles/Home.module.css'
+
+//const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+      <div>
+        <Head>
+            <title>Caleb Site</title>
+        </Head>
+        <h1>Hello World!</h1>
+        <p>I'm deployed in <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">Vercel</a></p>
+      </div>
+  )
+}
